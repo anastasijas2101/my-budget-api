de26010081f20b8d82b5395b3f54ab0afaccb734
anastasijas2101 <anastasijas101@gmail.com>
2024-08-16 11:03:23 +0200

    first commit

---
+++ src/main/java/com/anastasija/MyBudgetApi/MyBudgetApiApplication.java
@@ -0,0 +1,13 @@
+package com.anastasija.MyBudgetApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBudgetApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBudgetApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anastasija/MyBudgetApi/MyBudgetApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anastasija.MyBudgetApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBudgetApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
